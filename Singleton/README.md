ab90f840874fcaac9813b2855d718767d74b6e6c
RabitBox <gixa.aturakka+gitlab@gmail.com>
2019-08-28 04:35:57 +0900

    Singleton追加

---
+++ Singleton/README.md
@@ -0,0 +1,6 @@
+# Singleton
+Singletonパターンを使用するクラスの継承元。
+
+# SingletonMonoBehavior
+MonoBehaviorを継承したSingletonパターンを使用するクラスの継承元。  
+Unityの機能に依存する。
---
+++ Singleton/Script/SingletonMonoBehavior.cs
@@ -0,0 +1,36 @@
+﻿//--------------------------------------------------
+// MonoBehaviorSingleton継承元クラス
+//--------------------------------------------------
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class SingletonMonoBehavior<T> : MonoBehaviour 
+	where T : SingletonMonoBehavior<T>, new()
+{
+	protected static GameObject managerObject;
+
+	protected static T _instance = null;
+	public static T Instance
+	{
+		get
+		{
+			if(_instance == null)
+			{
+				// エディタ上で設定されていないか確認
+				_instance = FindObjectOfType <T>();
+
+				// なければGameObjectを生成しアタッチする
+				if (_instance == null)
+				{
+					if (managerObject == null)
+					{
+						managerObject = new GameObject("SingletonObject");
+					}
+
+					_instance = managerObject.AddComponent<T>();
+				}
+			}
+			return _instance;
+		}
+	}
+}
