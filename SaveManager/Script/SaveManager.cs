b5ffc6551aad8ec588cbb63080db880e2e2550f8
RabitBox <gixa.aturakka+gitlab@gmail.com>
2019-08-28 04:54:03 +0900

    テキストの読み込み・書き込み機能をテキスト全体と、行毎に読み込む機能に分割。

--- SaveManager/Script/SaveManager.cs
+++ SaveManager/Script/SaveManager.cs
@@ -6,23 +6,49 @@ using UnityEngine;
 /// </summary>
 public class SaveManager
 {
+	//--------------------------------------------------
+	// Save Methods
+	//--------------------------------------------------
 	/// <summary>
-	/// 渡したデータをセーブする。
+	/// 渡した文字列を保存する。
 	/// </summary>
 	/// <param name="content">保存するデータ。</param>
-	/// <param name="fileName">データを保存するファイル名。</param>
+	/// <param name="fileName">保存するファイル名。</param>
 	public static void Save(string content, string fileName = "save.dat")
 	{
 		File.WriteAllText(Path.Combine(Application.persistentDataPath, fileName), content);
 	}
 
 	/// <summary>
+	/// 文字列配列を行毎に保存する。
+	/// </summary>
+	/// <param name="contents">保存するデータ配列。</param>
+	/// <param name="fileName">保存するファイル名。</param>
+	public static void SaveLines(string[] contents, string fileName = "save.dat")
+	{
+		File.WriteAllLines(Path.Combine(Application.persistentDataPath, fileName), contents);
+	}
+
+	//--------------------------------------------------
+	// Load Methods
+	//--------------------------------------------------
+	/// <summary>
 	/// セーブしたデータを読み込む。
 	/// </summary>
-	/// <param name="fileName">保存したデータ名。</param>
+	/// <param name="fileName">読み込むデータの名前。</param>
 	/// <returns>読み込んだデータ。</returns>
 	public static string Load(string fileName = "save.dat")
 	{
 		return File.ReadAllText(Path.Combine(Application.persistentDataPath, fileName));
+	}
+
+	/// <summary>
+	/// セーブしたテキストデータを行毎に読み込む。
+	/// </summary>
+	/// <param name="fileName">読み込むデータの名前。</param>
+	/// <returns>読み込んだテキストデータの配列。</returns>
+	public static string[] LoadLines(string fileName = "save.dat")
+	{
+		return File.ReadAllLines(Path.Combine(Application.persistentDataPath, fileName));
 	}
 }
